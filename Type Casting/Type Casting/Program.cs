36ce8a36512cbebbe1fdcecb6c073c0683d0b27e
Daniel-Hercules <73712933+Daniel-Hercules@users.noreply.github.com>
2020-11-02 17:08:50 +0000

    Add files via upload

---
+++ Type Casting/Type Casting/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Type_Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implicit Casting - Done automatically 
+            // when passing from a smaller type to a
+            // larger type.
+
+            int myInt = 5;
+
+            double myDouble = myInt;
+
+            Console.WriteLine(myInt);
+            Console.WriteLine(myDouble);
+
+            // Explicit Casting - Done, manually by
+            // placing the type in parenttheses in 
+            // front of the value.
+
+            double anotherDouble = 2.34;
+
+            //This will only give the full number of the double.
+            
+            int anotherInt = (int)anotherDouble;
+
+            Console.WriteLine(anotherDouble);
+            Console.WriteLine(anotherInt);
+
+
+
+        }
+    }
+}
